850571877acca4fd1fc12bafc7d0f1635b04f22c
KenethUrrutia <kenethurrutia24d@gmail.com>
2024-09-05 19:24:25 -0600

    aggrega info

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MessageService } from '../../shared/services/message.service';
 
 @Component({
-  selector: 'app-data',
-  standalone: true,
-  imports: [],
-  templateUrl: './data.component.html',
-  styleUrl: './data.component.scss'
+	selector: 'app-data',
+	standalone: true,
+	imports: [],
+	templateUrl: './data.component.html',
+	styleUrl: './data.component.scss'
 })
 export class DataComponent {
+	@Input() dato: number = 0;
+	datoObservable: number = 0;
 
+	constructor(private messageService: MessageService) {
+		messageService.increment$.subscribe((resp) => {
+			this.datoObservable = resp;
+		});
+	}
+
+	ngOnInit(): void {}
 }
