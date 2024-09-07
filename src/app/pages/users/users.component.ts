e250132beb5a8c4951c4cca4d4133211d6c5ba3e
KenethUrrutia <kenethurrutia24d@gmail.com>
2024-09-07 08:38:42 -0600

    mat-table

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,16 +1,21 @@
 import { UserInterface } from '../../shared/interfaces';
 import { UserService } from './../../shared/services/user.service';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
 	selector: 'app-users',
 	standalone: true,
-	imports: [],
+	imports: [MatCardModule, MatButtonModule, MatProgressBarModule, MatTableModule],
 	templateUrl: './users.component.html',
 	styleUrl: './users.component.scss'
 })
 export default class UsersComponent {
-	users: UserInterface[] = [];
+	displayedColumns: string[] = ['id', 'first_name', 'last_name', 'email', 'avatar'];
+	dataSource: UserInterface[] = [];
 
 	constructor(private userService: UserService) {}
 
@@ -20,8 +25,7 @@ export default class UsersComponent {
 
 	getUsers() {
 		this.userService.getUsers().subscribe((resp) => {
-			this.users = resp.data;
-			console.log(this.users);
+			this.dataSource = resp.data;
 		});
 	}
 }
